ef950447992f0a8c47190f56e316e85453a26a47
zhangbo <313670184@qq.com>
2017-08-27 09:35:35 +0800

    去掉没用的引用

--- e3-manager-web/src/main/java/cn/e3/manager/controller/ItemController.java
+++ e3-manager-web/src/main/java/cn/e3/manager/controller/ItemController.java
@@ -1,8 +1,5 @@
 package cn.e3.manager.controller;
 
-import javax.servlet.ServletContext;
-import javax.servlet.http.HttpServletResponseWrapper;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
