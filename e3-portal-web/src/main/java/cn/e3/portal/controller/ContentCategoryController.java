741e8eadc081b2dc350f0ee255d6adb82e098ce7
zhangbo <313670184@qq.com>
2017-08-31 14:27:36 +0800

    第五天,广告

---
+++ e3-portal-web/src/main/java/cn/e3/portal/controller/ContentCategoryController.java
@@ -0,0 +1,19 @@
+package cn.e3.portal.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ContentCategoryController {
+	
+	/**
+	 * 加载树形分类
+	 * 参数
+	 * 返回值
+	 */
+	@RequestMapping("")
+	public void show(){
+		
+	}
+
+}
